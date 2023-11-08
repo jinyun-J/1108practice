4c007337d416e09b4806a1ecdba6b9b9b5321455
깃jinyun-J <ppp6289@gmail.com>
2023-11-09 01:19:28 +0900

    서비스 추가

--- src/main/java/org/example/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/org/example/wiseSaying/controller/WiseSayingController.java
@@ -5,25 +5,27 @@ import org.example.wiseSaying.entity.WiseSaying;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.example.wiseSaying.service.WiseSayingService;
 
 public class WiseSayingController {
-    long lastWiseSayingId = 0;
-    List<WiseSaying> wiseSayings = new ArrayList<>();
-
+    private final WiseSayingService wiseSayingService;
+    public WiseSayingController() {
+        wiseSayingService = new WiseSayingService();
+    }
     public void write() {
-        long id = lastWiseSayingId + 1; // id == 1
-
         System.out.print("명언 : ");
         String content = Container.getScanner().nextLine().trim();
         System.out.print("작가 : ");
         String author = Container.getScanner().nextLine().trim();
-        lastWiseSayingId = id; //lW == 1
-        WiseSaying wiseSaying = new WiseSaying(lastWiseSayingId, author, content);
-        wiseSayings.add(wiseSaying);
-        System.out.printf("%d번 명언이 등록되었습니다.", lastWiseSayingId);
+
+        long id = wiseSayingService.write(content, author);
+
+        System.out.printf("%d번 명언이 등록되었습니다.", id);
     }
 
     public void list() {
+        List<WiseSaying> wiseSayings = wiseSayingService.findByAll();
+
         System.out.println("번호 / 작가 / 명언");
         System.out.println("목록갯수는 " + wiseSayings.size() + "입니다");
         for (int i = wiseSayings.size() - 1; i >= 0; i--) {
@@ -38,7 +40,7 @@ public class WiseSayingController {
         long id = Long.parseLong(Container.getScanner().nextLine());
 
         // aticleList 입력받은 id 값이랑 같은 id 를 가지고있는 wiseSaying 객체 찾기
-        WiseSaying wiseSaying = this.getWiseSayingFindId(id);
+        WiseSaying wiseSaying = wiseSayingService.findById(id);
 
         // 작성자만 작성할 수 있도록
         // 게시글 존재하지 않을 경우
@@ -47,7 +49,7 @@ public class WiseSayingController {
             return;
         }
 
-        wiseSayings.remove(wiseSaying);
+        wiseSayingService.remove(wiseSaying);
         System.out.println(id + "번 명언이 삭제되었습니다");
     }
     public void modify() {
@@ -55,13 +57,13 @@ public class WiseSayingController {
         long id = Long.parseLong(Container.getScanner().nextLine());
 
         // aticleList 입력받은 id 값이랑 같은 id 를 가지고있는 wiseSaying 객체 찾기
-        WiseSaying wiseSaying = this.getWiseSayingFindId(id);
+        WiseSaying wiseSaying = wiseSayingService.findById(id);
 
         // 작성자만 작성할 수 있도록
         // 게시글 존재하지 않을 경우
         if (wiseSaying == null) {
             System.out.println("게시글이 존재하지 않습니다.");
-            return;
+            return; // 어떻게 명령으로 돌아기는지 디버깅해봤는데 모르겠음
         }
         System.out.printf("기존 작가) %s \n",wiseSaying.getAuthor());
         String author = Container.getScanner().nextLine();
@@ -70,18 +72,12 @@ public class WiseSayingController {
         System.out.printf("기존 명언) %s \n",wiseSaying.getContent());
         String content = Container.getScanner().nextLine();
         wiseSaying.setContent(content);
+
+        wiseSayingService.modify(wiseSaying, author, content);
+
         System.out.println(id + "번 명언이 수정되었습니다");
     }
 
-    private WiseSaying getWiseSayingFindId(long lastWiseSayingId) {
-        for (int i = 0; i < wiseSayings.size(); i++) {
-            WiseSaying wiseSaying = wiseSayings.get(i);
-            if (wiseSaying.getId() == lastWiseSayingId) {
-                return wiseSaying;
-            }
-        }
-        return null;
-    }
 }
 
 
